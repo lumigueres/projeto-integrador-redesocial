d236d8aac4466a71f9fa0f1974a9aea8e3a9349b
Luisa Migueres <lumigueres@gmail.com>
2018-11-12 21:47:31 -0200

    Atualizando footer

---
+++ componentes/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/footer.css">
+    <title></title>
+  </head>
+
+  <body>
+
+<div class="row">
+    <footer class="label label-primary col-md-12 col-xs-12 col-lg-12 main-footer">
+      <ul class="nav nav-pills">
+        <li role="presentation"><a href="#">QUEM SOMOS</a></li>
+        <li role="presentation"><a href="#">FAQ</a></li>
+      </ul>
+    </footer>
+  </div>
+
+  </body>
+</html>
