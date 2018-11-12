079503a97c6d40012f7608f99114867bddcc7df1
Luisa Migueres <lumigueres@gmail.com>
2018-11-12 21:48:51 -0200

    Atualizando home
    Área de login adicionada

--- componentes/index.php
+++ componentes/index.php
@@ -3,52 +3,62 @@
   <head>
     <meta charset="utf-8">
     <title>sociable - login</title>
-    <link rel="stylesheet" href="../css/login.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/login.css">
   </head>
   <body>
 
+
 <?php include 'header.php' ?>
 
-    <div class="container">
+<div class="container-fluid">
 
-      <img class="welcome side" src="../images/login_image.jpg">
+<div class="row team-image">
 
-      <div class="login-area col-md-4 col-xs-12 side">
+<figure class="welcome-image col-xs-12 col-md-8 col-lg-8">
+    <img class="welcome side" src="../images/login_image.jpg">
+    <figcaption class="label label-primary welcome-message">uma rede social sob medida para a sua equipe</figcaption>
+    <button class="faq-button">descubra como funciona</button>
+</figure>
 
-              <form id="cadastro" method="post" action="">
-                <h2>login</h2>
-
-                <div>
-                  <label for="nome_cad">Seu nome</label>
-                  <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="nome">
-                </div>
-
-                <div>
-                  <label for="email_cad">Seu e-mail</label>
-                  <input id="email_cad" name="email_cad" required="required" type="email" placeholder="contato@htmlecsspro.com">
-                </div>
-
-                <div>
-                  <label for="senha_cad">Sua senha</label>
-                  <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="ex. 1234">
-                </div>
-
-                <div>
-                  <input type="submit" value="Cadastrar">
-                </div>
-
-                <div class="link">
-                  Já tem conta?
-                  <a href="#paralogin"> Ir para Login </a>
-                </div>
-              </form>
-            </div>
-
+      <form class="form-horizontal login-area side login-box">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">e-mail</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="seu e-mail">
     </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">senha</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="sua senha">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox">Lembre de mim
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default faq-button entry-button">entrar</button>
+    </div>
+  </div>
+</form>
+
+</div>
+
+
+
+</div>
 
 <?php include 'footer.php' ?>
+
 
   </body>
 </html>
