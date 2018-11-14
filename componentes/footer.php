e957582e0a743cf7433e39dd3944cdb0adbd82f2
Luisa Migueres <lumigueres@gmail.com>
2018-11-14 04:25:01 -0200

    Associando CSS do footer

--- componentes/footer.php
+++ componentes/footer.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/footer.css">
-    <title></title>
-  </head>
 
-  <body>
+
+<link rel="stylesheet" href="../css/footer.css">
 
 <div class="row">
     <footer class="label label-primary col-md-12 col-xs-12 col-lg-12 main-footer">
