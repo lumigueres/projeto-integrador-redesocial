2e5e08ea309588ef91bb3a4b9d79e5f12e94e115
Luisa Migueres <lumigueres@gmail.com>
2018-11-30 11:55:21 -0200

    Corrigindo types de inputs (página de profile)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,28 +8,27 @@
 
 <div class="profile container-fluid">
 
+  <h1>{{ $user }}</h1>
 
   <div class="profile__row row">
 
     <div class="profile-picture col-lg-3 col-md-3 col-sm-8 col-sm-offset-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
 
       <img src="../images/profile_pic.png" alt="foto" class="profile-picture__image">
-      <button class="btn btn-default profile-picture__button" type="button" name="button">insira a sua foto</button>
+      <button class="btn btn-default profile-picture__button" type="button" name="button"><input type="file" accept="image/*,.pdf">insira a sua foto</button>
 
     </div>
 
     <div class="profile-input col-lg-9 col-md-9 col-sm-12 col-xs-12">
-
-      <input type="name" class="form-control profile-input__field" id="name" placeholder="nome">
-      <input type="last-name" class="form-control profile-input__field" id="last-name" placeholder="sobrenome">
-      <input type="area" class="form-control profile-input__field" id="area" placeholder="área de atuação">
-      <input type="email" pattern=".+@globex.com" class="form-control profile-input__field" id="email-corp" placeholder="e-mail">
-      <input type="position" class="form-control profile-input__field" id="position" placeholder="cargo/papel">
-      <input type="telephone" class="form-control profile-input__field" id="telephone" placeholder="telefone">
-      <input type="number" class="form-control profile-input__field" id="number" placeholder="ramal">
-      <input type="create-password" class="form-control profile-input__field" id="create-password" placeholder="crie a sua senha">
-      <input type="confirm-password" class="form-control profile-input__field" id="confirm-password" placeholder="confirme a sua senha">
-
+        <input type="text" class="form-control profile-input__field" id="name" placeholder="nome">
+        <input type="text" class="form-control profile-input__field" id="last-name" placeholder="sobrenome">
+        <input type="text" class="form-control profile-input__field" id="area" placeholder="área de atuação">
+        <input type="email" pattern=".+@globex.com" class="form-control profile-input__field" id="email-corp" placeholder="e-mail">
+        <input type="text" class="form-control profile-input__field" id="position" placeholder="cargo/papel">
+        <input type="number" class="form-control profile-input__field" id="telephone" placeholder="telefone">
+        <input type="number" class="form-control profile-input__field" id="number" placeholder="ramal">
+        <input type="text" class="form-control profile-input__field" id="create-password" placeholder="crie a sua senha">
+        <input type="text" class="form-control profile-input__field" id="confirm-password" placeholder="confirme a sua senha">
     </div>
 
   </div>
@@ -38,7 +37,7 @@
 
     <div class="profile-description col-lg-6 col-md-6 col-xs-12">
       <p class="profile-description__title">sobre mim</p>
-      <textarea class="form-control profile-description__text" rows="3" placeholder="escreva a sua mini bio"></textarea>
+      <textarea class="form-control profile-description__text" rows="3" placeholder="escreva a sua mini bio" maxlength="400"></textarea>
     </div>
 
     <div class="profile-tag-area col-lg-6 col-md-6 col-xs-12">
