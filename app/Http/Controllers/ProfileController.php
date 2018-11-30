713223b95c79c2ed961e1e3048a76d6d4709c6d7
Luisa Migueres <lumigueres@gmail.com>
2018-11-30 11:52:30 -0200

    Retornando infos de usuário quando logado

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
-    public function exibir(){
-        return view ('profile');
+    public function exibir($user_email){
+        $user = DB::table('usuario')
+          ->where("e-mail", urldecode($user_email))
+          ->get();
+
+        return view ('profile', [ 'user' => $user ]);
     }
 }
