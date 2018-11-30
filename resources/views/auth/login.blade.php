fb100dc7a0ba32f0a6974ecff7e10ece7717ba91
Luisa Migueres <lumigueres@gmail.com>
2018-11-30 11:53:17 -0200

    Retornando login autenticado com layout certo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,60 @@
+@php ($isNotLogged = true )
 @extends('layouts.app')
 
+@section('css-custom', '/css/login.css')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+  <div class="container-fluid">
+    <div class="row flex-md">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+      <div class="welcome col-md-8">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+        <img class="welcome__image" src="/images/login_image.jpg">
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div class="welcome__wrapper">
+          <p class="welcome__slogan">uma rede social sob medida para a sua equipe</p>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+          <!-- essa linha abaixo faz o link entre as paginas -->
+          <a class="btn btn-default welcome__button" href="/descubra" name="button">descubra como funciona</a>
         </div>
+
+      </div>
+
+      <div class="login col-md-4">
+
+        <div class="login__title">
+          <p>login</p>
+        </div>
+
+        <form class="form-horizontal side login-form" method="POST" action="{{ route('login') }}">
+          @csrf
+          <label for="email" class="control-label">e-mail</label>
+
+          <input type="email" class="form-control" id="email" name="email" required placeholder="seu e-mail">
+
+
+          <label for="password" class="control-label">senha</label>
+          <input type="password" class="form-control" id="password" name="password" placeholder="sua senha">
+
+          <div class="checkbox">
+            <label>
+              <input type="checkbox">Lembre de mim
+            </label>
+          </div>
+
+          <button type="submit" class="btn btn-default login__button">entrar</button>
+          <button type="button" class="btn btn-default login__button--register"><a href="/profile">cadastrar</a></button>
+
+        </form>
+      </div>
+
     </div>
+
+  </div>
+
 </div>
+
+<script type="text/javascript" src="/js/login.js"></script>
+
 @endsection
