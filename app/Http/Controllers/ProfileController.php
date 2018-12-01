46e1ba1dad5b9048cdb7d6456341386a6da4e8ab
Luisa Migueres <lumigueres@gmail.com>
2018-11-30 22:40:45 -0200

    Otimizando autenticação

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,11 +8,37 @@ use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
-    public function exibir($user_email){
-        $user = DB::table('usuario')
-          ->where("e-mail", urldecode($user_email))
+    public function exibirProfile($identifier, Request $request){
+        $logged_user = $request->user();
+        $user = DB::table('user_info')
+          ->where("email", urldecode($identifier))
           ->get();
 
-        return view ('profile', [ 'user' => $user ]);
+        if ($user->isEmpty()) {
+          return abort(404);
+        }
+
+        $isOwner = $user[0]->email == urldecode($logged_user->email);
+
+        return view ('profile', [ 'user' => $user[0], 'isOwner' => $isOwner]);
+    }
+
+    public function updateProfile($identifier, Request $request) {
+        if ($request->user()->email == urldecode($identifier)) {
+          $name = $request->name;
+          $last_name = $request->last_name;
+          $email = $request->email;
+          $phone = $request->phone;
+          $area = $request->area;
+          $position = $request->position;
+          $number = $request->number;
+//corrigir os nomes das colunas no banco
+
+          DB::table('user_info')
+            ->where("email", urldecode($identifier))
+            ->update(['name' => $name]);
+        }
+
+        return redirect('/profile/'.$identifier);
     }
 }
