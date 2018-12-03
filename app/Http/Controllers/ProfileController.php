7fbcf57843955716dfb04fc43b42beb14cadeacf
Luisa Migueres <lumigueres@gmail.com>
2018-12-02 23:02:15 -0200

    Criando armazenamento de fotos

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -32,11 +33,30 @@ class ProfileController extends Controller
           $area = $request->area;
           $position = $request->position;
           $number = $request->number;
+          $mini_bio = $request->mini_bio;
 //corrigir os nomes das colunas no banco
+
+// Storage::put('../public/images/uploaded/file.jpg', $image);
+          $user_info = [
+            'name' => $name,
+            'last_name' => $last_name,
+            'phone' => $phone,
+            'area' => $area,
+            'position' => $position,
+            'number' => $number,
+            'mini_bio' => $mini_bio,
+        ];
+
+        if($request->image != null && $request->image->isValid()) {
+          $image = $request->image->store('profile_images', ['disk' => 'public']);
+          $user_info['image'] = $image;
+        }
+
 
           DB::table('user_info')
             ->where("email", urldecode($identifier))
-            ->update(['name' => $name]);
+            ->update($user_info);
+
         }
 
         return redirect('/profile/'.$identifier);
